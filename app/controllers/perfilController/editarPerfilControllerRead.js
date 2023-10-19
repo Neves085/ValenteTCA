f7001a37f858fbe74202c64191ba1c95d98110e2
Pedro Neves <pedroneves188@gmail.com>
2023-10-18 21:31:52 -0300

    criação da page de editar perfil

---
+++ app/controllers/perfilController/editarPerfilControllerRead.js
@@ -0,0 +1,29 @@
+const usuarioModel = require("../../models/Usuario");
+const jwt = require("jsonwebtoken");
+
+class EditarPerfilController {
+    async getPage(req, res) {
+        const token = req.session.token;
+        const {userId} = jwt.decode(token, process.env.SECRET);
+
+        const user = await usuarioModel.findUserById(userId);
+
+
+
+    return res.render("pages/editar-perfil.ejs", {
+        data: {
+          page_name: "",
+          input_values: {
+            nome: user.nome,
+            nome_de_usuario: user.nome_de_usuario,
+            email: user.email,
+            telefone: user.telefone
+          },
+        },
+      });
+    };
+};
+    
+const editarPerfilControllerRead = new EditarPerfilController();
+
+module.exports = editarPerfilControllerRead;
---
+++ app/controllers/perfilController/editarPerfilControllerUpdate.js
@@ -0,0 +1,81 @@
+const usuarioModel = require("../../models/Usuario");
+const jwt = require("jsonwebtoken");
+
+class EditarPerfilController {	
+  constructor() {
+		this.editUser = this.editUser.bind(this);
+	}
+
+    async editUser(req, res) {
+    const token = req.session.token;
+    const {userId} = jwt.decode(token, process.env.SECRET);
+    const user = await usuarioModel.findUserById(userId);
+
+    const {nome, nome_de_usuario, email, telefone, logradouro, numero_residencial, bairro, 
+        cidade, estado, cep } = req.body;
+
+    try {
+		await usuarioModel.updatePerfil({
+            nome,
+            nome_de_usuario,
+            email,
+            telefone
+		}, userId);
+		return res.redirect("/perfil-user");
+		} catch (erro) {
+			console.log(erro);
+
+			if (erro.code === "P2002") {
+				return res.render("pages/editar-perfil.ejs", {
+					data: {
+						page_name: "Editar perfil",
+						input_values: {
+                            nome,
+                            nome_de_usuario,
+                            email,
+                            telefone,
+                            logradouro,
+                            numero_residencial,
+                            bairro,
+                            cidade,
+                            estado,
+                            cep
+						},
+						errors: {
+							email_error: {
+								msg: "Email já cadastrado!",
+							},
+						},
+					},
+				});
+			}
+
+			return res.render("pages/editar-perfil.ejs", {
+				data: {
+					page_name: "Editar perfil",
+					input_values: {
+                        nome,
+                        nome_de_usuario,
+                        email,
+                        telefone,
+                        logradouro,
+                        numero_residencial,
+                        bairro,
+                        cidade,
+                        estado,
+                        cep
+					},
+					errors: {
+						sistema_error: {
+							msg: "Erro de sistema, tente novamente mais tarde!",
+						},
+					},
+				},
+			});
+		}
+    }
+}
+
+const editarPerfilControllerUpdate = new EditarPerfilController()
+
+module.exports = editarPerfilControllerUpdate;

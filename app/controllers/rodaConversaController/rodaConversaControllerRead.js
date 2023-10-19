d76bc5e974fc2f6c146dceae23218b5e8b9677c4
Pedro Neves <pedroneves188@gmail.com>
2023-10-19 03:42:30 -0300

    html

--- app/controllers/rodaConversaController/rodaConversaControllerRead.js
+++ app/controllers/rodaConversaController/rodaConversaControllerRead.js
@@ -1,6 +1,6 @@
 class RodaDeConversaController {
     returnPage(req, res) {
-        return res.render("pages/rodas-de-conversa", {
+        return res.render("pages/roda-de-conversa", {
             data: {
                 page: "Roda de conversa"
             }

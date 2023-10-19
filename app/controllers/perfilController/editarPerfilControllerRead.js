cc88e8b86d80feecd00199e32917b125a3e9d4f0
Pedro Neves <pedroneves188@gmail.com>
2023-10-19 02:21:03 -0300

    dwee

--- app/controllers/perfilController/editarPerfilControllerRead.js
+++ app/controllers/perfilController/editarPerfilControllerRead.js
@@ -2,27 +2,29 @@ const usuarioModel = require("../../models/Usuario");
 const jwt = require("jsonwebtoken");
 
 class EditarPerfilController {
-    async getPage(req, res) {
-        const token = req.session.token;
-        const {userId} = jwt.decode(token, process.env.SECRET);
+  async getPage(req, res) {
+    try {
+      const token = req.session.token;
+      const { userId } = jwt.decode(token, process.env.SECRET);
+      const user = await usuarioModel.findUserById(userId);
 
-        const user = await usuarioModel.findUserById(userId);
-
-
-
-    return res.render("pages/editar-perfil.ejs", {
+      return res.render("pages/editar-perfil.ejs", {
         data: {
-          page_name: "",
+          page_name: "Editar Perfil",
           input_values: {
             nome: user.nome,
             email: user.email,
             telefone: user.telefone
-          },
-        },
+          }
+        }
       });
-    };
-};
-    
+    } catch (error) {
+      console.error("Erro ao buscar e renderizar página de edição de perfil:", error);
+      return res.status(500).json({ error: "Erro ao buscar e renderizar página de edição de perfil." });
+    }
+  }
+}
+
 const editarPerfilControllerRead = new EditarPerfilController();
 
 module.exports = editarPerfilControllerRead;

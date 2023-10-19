ec7d7ae3c4125a08a2615431b52c01a3a01f1df6
Pedro Neves <pedroneves188@gmail.com>
2023-10-19 09:40:01 -0300

    dcdcd

--- app/controllers/loginController/loginControllerAuth.js
+++ app/controllers/loginController/loginControllerAuth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 class LoginController {
     async autorizarUsuario(req, res) {
         const token = jwt.sign({ userId:use.id},process.env.SECRET);
-        const loginRedirectUrl = req.session.loginRedirectUrl ? req.session.loginRedirectUrl : "/perfil";
+        const loginRedirectUrl = req.session.loginRedirectUrl ? req.session.loginRedirectUrl : "/rodas-de-conversa";
         req.session.token = token;
         req.session.loginRedirectUrl = null;
         return res.redirect(loginRedirectUrl);

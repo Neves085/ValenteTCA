eff6470665f3af47a24c85820215112f5ee059e9
Pedro Neves <pedroneves188@gmail.com>
2023-10-19 07:34:14 -0300

    fhfhf

--- app/controllers/homeperfilController/perfilControllerRead.js
+++ app/controllers/homeperfilController/perfilControllerRead.js
@@ -1,8 +1,17 @@
+const usuarioModel = require("../../models/Usuario");
+const jwt = require("jsonwebtoken");
+
 class perfilController {
-    returnPage(req, res) {
+    async returnPage(req, res) {
+        const token = req.session.token;
+        const {userId} = jwt.decode(token, process.env.SECRET);
+        const usuario = await usuarioModel.findUserById(userId);
+
+
         return res.render("pages/perfil.ejs", {
             data: {
-                page: "perfil"
+                page_name: "Perfil",                
+                usuario
             }
         })
     }

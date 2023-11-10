5a75db3ee2dea792fa1970dc8235247da055d45b
Vinícius Gonçalves <vinicius.devlp@gmail.com>
2023-11-10 09:16:39 -0300

    alterações finais

--- prisma/migrations/20231110113518_nome_unico/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[nome]` on the table `Sessao` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Sessao_nome_key` ON `Sessao`(`nome`);
